6e2b9ac9e05de3c9b5668a3a16d3f70fe931c604
Rajveer Choubisa <ranveer0596@gmail.com>
2025-04-14 16:39:46 +0530

    backend: authroutes,userModel,authcontroller ,dbjs,indexjs

---
+++ backend/controllers/authController.js
@@ -0,0 +1,48 @@
+import User from "../models/userModels.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existing = await User.findOne({ email }); //finding user by email
+
+    if (existing) return res.status(400).json({ msg: "User already exists" }); //checking if user exist or not 
+
+    //now password hashing for new user
+    const hashed = await bcrypt.hash(password,10);
+    //new user creation with hashed password store
+    const newUser = await User.create({name,email,password:hashed});
+
+    res.status(201).json({msg:"User created Successfully",user:newUser});
+
+  } catch (error) {
+    res.status(500).json({msg:"Registration error",error:error.message});
+  }
+};
+
+export const loginUser = async(req,res) =>{
+    const{email,password} = req.body;
+
+    try {
+        //finding user
+        const user = await User.findOne({email});
+//exist or not
+        if(!user) return res.status(404).json({msg:'User not found'});
+
+
+        //password matching
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch) return res.status(401).json({msg: "Invalid Credentials"});
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
+
+        res.json({token , user});
+    } catch (error) {
+        res.status(500).json({msg:'Login error',error:error.message});
+    }
+};
+
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express"
-import mongoose from "mongoose"
+import mongoose, { connect } from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 
 dotenv.config();
 
@@ -9,10 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
+
+
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true})
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
+app.use('/api/auth', authRoutes);
 app.get('/',(req,res) => res.send('Zidio API running'));
 
 app.listen(process.env.PORT || 5000, () => {
